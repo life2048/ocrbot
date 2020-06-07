88674b7b3971a0c9d7df0d098bf58447faa4eb29
life2048 <52625812+life2048@users.noreply.github.com>
2020-06-07 12:49:38 +0530

    Update ocrbot.py

--- ocrbot.py
+++ ocrbot.py
@@ -21,14 +21,9 @@ def start(bot, update):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi! \n\nWelcome to Optical Character Recognizer Bot. \n\nJust send a clear image to the bot and it will recognize the text in the image and send it as a message!\nTo get my contact details tap /contact \nTo get donation details tap /donate\n')
 
-def donate(bot, update):
-    """Send a message when the command /donate is issued."""
-    bot.send_message(chat_id=201566591,text=str(update.message.chat_id)+" "+str(update.message.from_user.name)+' clicked /donate ')
-    update.message.reply_text("To donate you can send any amount you wish to me using the following *Payment Options*:\n\nUPI - `sumanjay@ybl` (Long press to copy)\n[Paytm](https://p-y.tm/SnE-jE9)\n[PayPal](https://paypal.me/sumanjay/10USD)\n[Debit/Credit/Netbanking/Other Wallets](https://www.instamojo.com/@sumanjay)\n\nAfter making payment don't forget to send a screenshot of the Transaction to @cyberboysumanjay", parse_mode=ParseMode.MARKDOWN)
-
 def contact(bot, update):
     """Send a message when the command /contact is issued."""
-    update.message.reply_text("Heya! You can find me on \n[Telegram](https://telegram.me/cyberboysumanjay)\n[Facebook](https://facebook.com/imsumanjay)\n[Instagram](https://instagram.com/sumanjay_)\n[Twitter](https://twitter.com/cyberboysj)\n[LinkedIn](https://www.linkedin.com/in/sumanjay-9816a4128)", parse_mode=ParseMode.MARKDOWN)
+    update.message.reply_text("Heya! You can find me on \n[Telegram](https://telegram.me/decomposed)", parse_mode=ParseMode.MARKDOWN)
 
 def search(bot, update):
     """Send reply of user's message."""
@@ -50,11 +45,10 @@ def error(bot, update, error):
 
 def main():
     """Start the bot."""
-    ocr_bot_token=os.environ.get("BOT_TOKEN", "")
+    ocr_bot_token=os.environ.get("1176613970:AAFb8BiOpY_N-LpQMu357apMIQajtWexvOU", "")
     updater = Updater(ocr_bot_token)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("donate", donate))
     dp.add_handler(CommandHandler("contact", contact))
     dp.add_handler(MessageHandler(Filters.photo, search))
     dp.add_error_handler(error)
